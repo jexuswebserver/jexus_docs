eeb2be8a40ce35779f862753c0c9062398318f4b
Lex Li <support@lextm.com>
2020-01-13 10:15:39 -0500

    Disabled edit_on_github.py.

--- manager/conf.py
+++ manager/conf.py
@@ -31,8 +31,7 @@ sys.path.insert(0, os.path.abspath('_ext'))
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.intersphinx',
-    'edit_on_github'
+    'sphinx.ext.intersphinx'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -384,10 +383,6 @@ feed_skip_regex = '(.)*index'
 feed_base_url = 'https://www.jexusmanager.com/en/latest'
 feed_description = 'Jexus Manager Documentation'
 feed_author = 'Lex Li'
-
-if not on_rtd:
-    edit_on_github_project = 'jexuswebserver/jexus_docs'
-    edit_on_github_branch = 'master/manager'
 
 def setup(app):
     on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
